418e9f8df385fd170accd1fa8339dd48fc700318
Elijah Ofori <kobbyofori9@gmail.com>
2023-06-01 21:05:36 +0000

    Practiced react-component-life-cycle and API calls with class and functional components

---
+++ src/components/UsersClass.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+
+export class UsersClass extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+    };
+  }
+
+  // Birth stage- call method after component is rendered
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("response:",data); //logging response to console to check data availability
+        this.setState({ users: data }); //updating users property in the state with the list of users retrieved from the API.
+        console.log("updated users:", this.state.users); //logging the state tothe console to check data(list of users) availability
+      })
+      // .catch((error) => {
+      //   console.log("Fetched error:", error);
+      // });
+  }
+  render() {
+    return <div><h3>UsersClass</h3>
+      {this.state.users.map(user=>(
+        <div key={user.id}>
+        <ul>
+          <li>Name: {user.name}</li>
+          <li>Username: {user.username}</li>
+          <li>Email: {user.email}</li>
+        </ul>
+        </div>
+        ))}
+    </div>;
+  }
+}
+
+export default UsersClass;
